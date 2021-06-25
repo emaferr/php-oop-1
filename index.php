13ac7f1cc4d7d2400af361a0898fe46da59338b5
ema.ferr <78190534+emaferr@users.noreply.github.com>
2021-06-25 17:01:51 +0200

    Use getimagesize () to verify URLs

--- index.php
+++ index.php
@@ -23,17 +23,17 @@ class Movie
  // return default image if the path is not present
  public function getPath() 
     {   
-        $checkImage = getimagesize($path);
+        $checkImage = getimagesize($this->path);
 
         if(!$checkImage){
             return $this->path = 'https://upload.wikimedia.org/wikipedia/commons/c/cd/Immagine_non_disponibile.JPG';
         }else{
-            return $this->path = $path;
-        }     
+            return $this->path;
+        }   
     }   
 }
 $movies = [
-    new Movie ('https://pad.mymovies.it/filmclub/2016/11/201/locandna.jpg','Deadpool 1','Inizialmente alle prese con la sua solita staticità di esistenza. Un giono però incontra e si innamora di una prostituta, Vanessa. La sua vita comincia a cambiare e sembra andare per il meglio, fin quando non scopre di avere un cancro all’ultimo stadio. Si lascia convincere a prendere parte ad un esperimento genetico, che gli causa un mutamento facciale. Si trasforma così in Deadpool, un anti-eroe con due finalità: uccidere Ajax che lo ha convinto sulla sperimentazione e cosa più importante trovare la forza per farsi vedere da Vanessa.',2016,8.5),
+    new Movie ('https://pad.mymovies.it/filmclub/2016/11/201/locandina.jpg','Deadpool 1','Inizialmente alle prese con la sua solita staticità di esistenza. Un giono però incontra e si innamora di una prostituta, Vanessa. La sua vita comincia a cambiare e sembra andare per il meglio, fin quando non scopre di avere un cancro all’ultimo stadio. Si lascia convincere a prendere parte ad un esperimento genetico, che gli causa un mutamento facciale. Si trasforma così in Deadpool, un anti-eroe con due finalità: uccidere Ajax che lo ha convinto sulla sperimentazione e cosa più importante trovare la forza per farsi vedere da Vanessa.',2016,8.5),
     new Movie ('https://pad.mymovies.it/filmclub/2016/11/201/locandina.jpg','Deadpool 2',"Il mercenario malato di cancro e trasformato in un essere pressoché immortale, capace di rigenerarsi da ogni ferita, si gode finalmente la vita insieme alla compagna Vanessa. Ma ad accettare irresponsabilmente, com'è nel suo stile, missioni da sicario in giro per tutto il mondo si finisce per farsi dei nemici e arriva presto per Deadpool il momento di pagare il conto. Una batosta tale da ritrovarsi a casa degli X-Men, con Colosso che ancora una volta gli dà la possibilità di essere un eroe e lo porta con sé in una missione per calmare un giovanissimo e potente mutante. Prevedibilmente le cose non vanno a finir bene e Deadpool si ritrova nei guai insieme al ragazzino a cui però si sta affezionando tanto che, quando dal futuro giungerà un letale guerriero deciso a ucciderlo, il loquace ex mercenario farà tutto il possibile per proteggere il giovane.",2018,8.2),
 ];
 var_dump($movies);
